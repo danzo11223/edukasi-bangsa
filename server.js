efdce66af0935ae0fb52b5374ec31cc3329613fa
NamaKamu <emailkamu@example.com>
2026-08-27 19:38:39 +0700

    Fix server for Render

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 
-app.listen(PORT, () => {
-  console.log("EDUKASI BANGSA jalan di:");
-  console.log("http://localhost:3000");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Edukasi Bangsa jalan di port ${PORT}`);
 });
